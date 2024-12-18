7aa2e9914dcd51e73cfdfb8f852d14e93c485d06
Yago García Martínez <yagogarmar@campus.monlau.com>
2024-12-18 20:16:25 +0100

    Final

--- Parte 2/server.cpp
+++ Parte 2/server.cpp
@@ -8,56 +8,67 @@
 std::vector<SOCKET> clients;
 std::mutex clients_mutex;
 std::vector<int> clientes;
-void setTextColor(int color) {
+void setTextColor(int color)
+{
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); // Obtiene el manejador de la consola
-    SetConsoleTextAttribute(hConsole, color);         // Cambia el color del texto
+    SetConsoleTextAttribute(hConsole, color);          // Cambia el color del texto
 }
-std::map<int, std::string> numeroMensaje; // Mapa para asociar números con mensajes
-std::string mensajes[] = {"1", "2", "3"}; // Los tres mensajes
+std::map<int, std::string> numeroMensaje;                 // Mapa para asociar números con mensajes
+std::string mensajes[] = {"1", "2", "3"};                 // Los tres mensajes
 int numMensajes = sizeof(mensajes) / sizeof(mensajes[0]); // Cantidad de mensajes disponibles
-int index = 0; // Índice del mensaje actual
+int index = 0;                                            // Índice del mensaje actual
 
-void handle_client(SOCKET clientSocket) {
+void handle_client(SOCKET clientSocket)
+{
     char buffer[1024];
 
-    while (true) {
+    while (true)
+    {
         int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesReceived <= 0) {
+        if (bytesReceived <= 0)
+        {
             closesocket(clientSocket);
             break;
         }
 
         buffer[bytesReceived] = '\0';
+        // GESTION DE LOS COLORES DE LOS CLIENTES >_<
 
+        if (numeroMensaje.find(clientSocket) == numeroMensaje.end())
+        {
+            // Si no tiene, asignar el siguiente mensaje disponible
+            numeroMensaje[clientSocket] = mensajes[index];
+            index = (index + 1) % numMensajes; // Ciclar entre los mensajes
+        }
+        if (numeroMensaje[clientSocket] == "1")
+        {
+            setTextColor(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        }
+        else if (numeroMensaje[clientSocket] == "2")
+        {
+            setTextColor(FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        }
+        else
+        {
+            setTextColor(FOREGROUND_RED | FOREGROUND_INTENSITY);
+        }
+
+        std::cout << "Mensaje de " << clientSocket << ": " << buffer << std::endl;
+        setTextColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
         // Retransmet el missatge a tots els clients
         std::lock_guard<std::mutex> lock(clients_mutex);
-        for (SOCKET client : clients) {
-            if (client != clientSocket) {
+        for (SOCKET client : clients)
+        {
+            if (client != clientSocket)
+            {
                 send(client, buffer, bytesReceived, 0);
-                // GESTION DE LOS COLORES DE LOS CLIENTES >_<
-                
-                if (numeroMensaje.find(clientSocket) == numeroMensaje.end()) {
-                    // Si no tiene, asignar el siguiente mensaje disponible
-                    numeroMensaje[clientSocket] = mensajes[index];
-                    index = (index + 1) % numMensajes; // Ciclar entre los mensajes
-                }
-                if(numeroMensaje[clientSocket] == "1"){
-                     setTextColor(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-                }else if (numeroMensaje[clientSocket] == "2"){
-                    setTextColor(FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-                }else{
-                    setTextColor(FOREGROUND_RED | FOREGROUND_INTENSITY);
-
-                }
-
-                std::cout << "Mensaje de " << clientSocket << ": " << buffer << std::endl;
-                setTextColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
             }
         }
     }
 }
 
-int main() {
+int main()
+{
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
@@ -67,15 +78,16 @@ int main() {
     serverAddr.sin_port = htons(12345);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    bind(serverSocket, (sockaddr *)&serverAddr, sizeof(serverAddr));
     listen(serverSocket, SOMAXCONN);
 
     std::cout << "Servidor actiu al port 12345" << std::endl;
 
-    while (true) {
+    while (true)
+    {
         sockaddr_in clientAddr;
         int clientSize = sizeof(clientAddr);
-        SOCKET clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientSize);
+        SOCKET clientSocket = accept(serverSocket, (sockaddr *)&clientAddr, &clientSize);
 
         std::lock_guard<std::mutex> lock(clients_mutex);
         clients.push_back(clientSocket);
